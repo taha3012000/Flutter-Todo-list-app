34eaf5a3c16150263c2a00ab087f12949613fdf6
Mohamed Taha <70654656+taha3012000@users.noreply.github.com>
2021-10-31 04:09:42 +0200

    Update new.dart

--- TODO/modules/new.dart
+++ TODO/modules/new.dart
@@ -1,4 +1,4 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/TODO/layout/cubit/cubit.dart';
 import 'package:todo/TODO/layout/cubit/states.dart';
@@ -20,4 +20,4 @@ class NewTasksScreen extends StatelessWidget {
       },
     );
   }
-*/
+}
