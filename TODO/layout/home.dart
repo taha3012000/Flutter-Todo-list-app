3d3e5143bbaa67329aa02df63078cc2cf806d09f
Mohamed Taha <70654656+taha3012000@users.noreply.github.com>
2021-10-31 04:07:52 +0200

    Update home.dart

--- TODO/layout/home.dart
+++ TODO/layout/home.dart
@@ -1,4 +1,4 @@
-/*import 'package:conditional_builder/conditional_builder.dart';
+import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -186,4 +186,4 @@ class HomeLayout extends StatelessWidget {
       ),
     );
   }
-*/
+}
