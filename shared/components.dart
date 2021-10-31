82f5107ea25c2630502394abf497d2f9d5f1c07e
Mohamed Taha <70654656+taha3012000@users.noreply.github.com>
2021-10-31 04:10:24 +0200

    Update components.dart

--- shared/components.dart
+++ shared/components.dart
@@ -1,4 +1,4 @@
-/*
+
 import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -259,4 +259,4 @@ Widget tasksBuilder({
       ],
     ),
   ),
-)*/
+}
